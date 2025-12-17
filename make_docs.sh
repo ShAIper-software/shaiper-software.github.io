1a8d5b5b341b5c62c583bc44819bd7eef2c411be
Michal Kravčenko <michal.kravcenko@vsb.cz>
2025-12-17 12:59:05 +0100

    cleanup of the repo

--- make_docs.sh
+++ make_docs.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-git submodule add https://code.it4i.cz/kra568/ai-surrogate-modelling submodules/ai-for-surrogate-modelling
-git submodule update --init --recursive
-git submodule update --remote
-
 . $ai_for_surrogate_modelling_path/bin/activate
 
 rsync -av --progress  submodules/ai-for-surrogate-modelling/docs/docs . --exclude='scripts'
